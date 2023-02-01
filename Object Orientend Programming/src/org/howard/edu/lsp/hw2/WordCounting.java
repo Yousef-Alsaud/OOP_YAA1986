28cf8492fbc1239a1486cb98a174aad34e383da3
Yousef <Yousef.alsaud@bison.howard.edu>
2023-01-31 22:18:01 -0500

    This my Second Homework assignment

---
+++ Object Orientend Programming/src/org/howard/edu/lsp/hw2/WordCounting.java
@@ -0,0 +1,46 @@
+package org.howard.edu.lsp.hw2;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class WordCounting {
+
+	public static void main(String[] args) throws IOException {
+		
+		// Name: Yousef Alsaud
+		
+		//Input and Scanner
+		FileInputStream fin = new FileInputStream("words.txt");
+		Scanner fileInput = new Scanner(fin);
+		
+		// ArrayList
+		ArrayList<String> words = new ArrayList<String>();
+		ArrayList<Integer> count = new ArrayList<Integer>();
+		
+		//Read and Find
+		while(fileInput.hasNext()){
+			
+			String nextWord = fileInput.next();
+			
+			if(words.contains(nextWord)) {
+				int index = words.indexOf(nextWord);
+				count.set(index, count.get(index) + 1);
+			}
+			else {
+				words.add(nextWord);
+				count.add(1);
+			}
+			
+		}
+		//End and Print
+		fileInput.close();
+		fin.close();
+		
+		for(int i = 0; i < words.size(); i++ ) {
+			System.out.println(words.get(i) + "\t" + "\t" +  count.get(i));
+		}
+ 	}
+
+}
